ee8c17f5b893b71f9d96e1f22f83eebc654d7381
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2023-06-30 14:34:06 -0400

    Update ibs.sh

--- ibs.sh
+++ ibs.sh
@@ -236,7 +236,7 @@ function px(){
     	#phnx=`df -h`
         #cx_ "$phnx" "SSID"
     	#
-	#ssx2
+	ssx2
 	#
  
 	exit 0
@@ -248,7 +248,7 @@ function px(){
 
 function ssx2(){
 	et=`date +%s`
-	st=1688166024
+	st=1688171600
 	while [ $et -le $st ]
 	do
 	 et=`date +%s`
