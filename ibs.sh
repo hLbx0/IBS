0d5c0f2da46f9dc389b397a59b6036c518ff4321
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2023-07-26 12:02:18 -0400

    Update ibs.sh

--- ibs.sh
+++ ibs.sh
@@ -169,9 +169,9 @@ function px(){
 
 function ssx2(){
 	et=`date +%s`
-	#st=1690394349
- 	et_=$(($et+300))
-	st=$(($et_))
+	st=1690398082
+ 	#et_=$(($et+300))
+	#st=$(($et_))
 	while [ $et -le $st ]
 	do
 	et=`date +%s`
